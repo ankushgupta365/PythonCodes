a2676b553c6f211d802e6435e1d9f05a457e5fd3
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2023-09-26 11:26:39 +0530

    Time: 86 ms (55.87%), Space: 16.9 MB (37.98%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -2,15 +2,20 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = defaultdict(list)
         
+        #constructiong adjacency list representation of the graph in reverse, to make sure we pop least lexical scope destination first
         for src, dst in sorted(tickets, reverse=True):
             graph[src].append(dst)
             
         itinerary = []
+
+        #go deep into the graph until no destination found from the source, this will ensure that we visit that destion last which don't have any source
         def dfs(airport):
             while graph[airport]:
-                dfs(graph[airport].pop())
+                dfs(graph[airport].pop())  #popping least lexical scope airport and visiting it
             itinerary.append(airport)
         
+
+        #kickstart the journey
         dfs("JFK")
         
         return itinerary[::-1]
