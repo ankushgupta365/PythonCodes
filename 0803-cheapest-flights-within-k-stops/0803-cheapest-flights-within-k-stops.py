5125671ae91fe988433e38aec750d5327e6f2f19
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2023-09-29 08:21:51 +0530

    Time: 103 ms (77.93%), Space: 17.6 MB (58.42%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,19 +1,21 @@
 class Solution:
-    def findCheapestPrice(self, n: int, edges: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = [[] for _ in range(n)]
-        m = len(edges)
-        for i in range(m):
-            u, v, w = edges[i]
-            adj[u].append((v, w))
-
-        @lru_cache(None)
-        def dp(u, k):
-            if u == dst: return 0 
-            if k == 0: return math.inf 
-            ans = math.inf 
-            for v, w in adj[u]: 
-                ans = min(ans, w+dp(v, k-1))
-            return ans 
-
-        ans = dp(src, k+1)
-        return ans if ans != math.inf else -1 
+    def findCheapestPrice(self, n: int, grid: List[List[int]], src: int, dst: int, k: int) -> int:
+        #constructing adjacency matrix
+        adj=[[]for i in range(n)]
+        for i in grid:
+            adj[i[0]].append([i[1],i[2]])
+        dist=[float('inf')for i in range(n)]
+        dist[src]=0
+        q=[[0,src,0]]
+        while q:
+            stp,node,cost=q.pop(0)
+            if stp>k:
+                continue
+            for i in adj[node]:
+                adjnode,fcost=i[0],i[1]
+                if dist[adjnode]>cost+fcost and stp<=k:
+                    dist[adjnode]=cost+fcost
+                    q.append([stp+1,adjnode,cost+fcost])
+        if dist[dst]==float('inf'):
+            return -1
+        return dist[dst]
