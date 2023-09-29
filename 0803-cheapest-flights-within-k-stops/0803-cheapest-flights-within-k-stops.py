7c12842be1d4d713f0f7ee6adf1207c940cbc7fb
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2023-09-29 08:27:07 +0530

    Time: 100 ms (85.72%), Space: 17.5 MB (91.99%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -6,16 +6,16 @@ class Solution:
             adj[i[0]].append([i[1],i[2]])
         dist=[float('inf')for i in range(n)]
         dist[src]=0
-        q=[[0,src,0]]
+        q=[[0,src,0]]   #[stops, node, toatlcost]
         while q:
-            stp,node,cost=q.pop(0)
+            stp,node,cost=q.pop(0)  #popping from left, alternatively u you can use dequeu()
             if stp>k:
                 continue
-            for i in adj[node]:
-                adjnode,fcost=i[0],i[1]
-                if dist[adjnode]>cost+fcost and stp<=k:
+            for elm in adj[node]:
+                adjnode,fcost=elm[0],elm[1]
+                if dist[adjnode]>cost+fcost and stp<=k:  # <= check means we are taking into considerating all the stops, bcz we are not including the staring and the ending stops
                     dist[adjnode]=cost+fcost
-                    q.append([stp+1,adjnode,cost+fcost])
+                    q.append([stp+1,adjnode,cost+fcost])   #increasing one stop
         if dist[dst]==float('inf'):
             return -1
         return dist[dst]
