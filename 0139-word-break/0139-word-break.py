7a15e4af7dcd529cd84f1ecf1dce10152213f109
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2023-10-04 23:56:27 +0530

    Time: 46 ms (46.27%), Space: 16.1 MB (99.22%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -4,6 +4,13 @@ class Solution:
         dp = [False] * (len(s)+1)
         dp[len(s)] = True   #base case, since "" is found anywhere
 
+        """
+            checking from end does, word match with the string and marking dp[i] true if it 
+            does, and in future when traverse further left we will store the same value at dp
+            [i+len(w)] which will ensure that at that position in the past some word from 
+            there has also matched
+        """
+
         for i in range(len(s)-1, -1, -1):
             for w in wordDict:
                 if (i + len(w)) <= len(s) and s[i: i+len(w)] == w:
