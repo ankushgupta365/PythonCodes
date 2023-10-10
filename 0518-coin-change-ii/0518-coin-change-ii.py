e917154c0c6e8b9b96ab89d99bb90b073fefd43a
Ankush Gupta <86787266+ankushgupta365@users.noreply.github.com>
2023-10-10 12:28:39 +0530

    Time: 257 ms (51.63%), Space: 16.7 MB (57.41%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        #O(n*m) and space O(n*m)
-        cache = {}
-        def dfs(i, a):
-            if a == amount:
-                return 1
-            if a > amount:
-                return 0
-            if i >= len(coins):
-                return 0
-            if (i,a) in cache:
-                return  cache[(i,a)]
-            
-            cache[(i,a)] = dfs(i, a+coins[i])+ dfs(i+1,a)
-            return cache[(i,a)]
-        return dfs(0,0)
+        #O(n*m) and space O(n)
+        dp = [0] * (amount+1)
+        dp[0] = 1  #base case
+
+        for i in range(len(coins)-1, -1, -1):
+            newDp = [0] * (amount+1)
+            newDp[0] = 1
+
+            for a in range(1, amount+1):
+                newDp[a] = dp[a]
+                if a - coins[i] >= 0:
+                    newDp[a]+=newDp[a - coins[i]]
+            dp = newDp
+        return dp[amount]
